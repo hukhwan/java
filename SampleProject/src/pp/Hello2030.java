8f3039844dad7e643c45af6036c8c52c9a143b32
hukhwan <hukhwan@gmail.com>
2021-06-07 20:23:37 +0900

    java_practice

---
+++ SampleProject/src/pp/Hello2030.java
@@ -0,0 +1,21 @@
+package pp;
+public class Hello2030 {
+    public static int sum(int n, int m) {
+    	return n + m;
+    }
+    
+    public static void main(String[] args) {
+    	int i = 20;
+    	int s;
+    	char a;
+    	
+    	s = sum(i,10);
+    	a = '?';
+    	System.out.println(a);
+    	System.out.println("Hello");
+    	System.out.println(s);
+    }
+}
+/*this is
+ 주석*/
+//주석
---
+++ SampleProject/src/pp/hi.java
@@ -0,0 +1,7 @@
+package pp;
+
+public class hi {
+    public static void main(String[] args) {
+    	System.out.print("Hi");
+    }
+}
