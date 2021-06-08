06aaeb05f63ae2247b9dbf52d8910e00a4873171
hukhwan <hukhwan@gmail.com>
2021-06-08 20:06:06 +0900

    login page

---
+++ BB/src/main/java/user/UserDAO.java
@@ -0,0 +1,57 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+	private Connection conn; //java -- connection --> DB
+	private PreparedStatement pstmt;  //query 대기 & 설정
+	private ResultSet rs;  //결과값 받기
+	
+	public UserDAO(){
+		try {
+			String dbURL = "jdbc:mariadb://localhost:3306/BB";
+			String dbID = "root";
+			String dbPassword = "803100";
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//로그인 영역
+	public int login(String userID, String userPassword) {
+		String sql = "select userPassword from user where userID = ?";
+		try {
+			pstmt = conn.prepareStatement(sql); //sql쿼리문을 대기 시킨다
+			pstmt.setString(1, userID); //첫번째 '?'에 매개변수로 받아온 'userID'를 대입
+			rs = pstmt.executeQuery(); //쿼리를 실행한 결과를 rs에 저장
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) {
+					return 1; //로그인 성공
+				}else
+					return 0; //비밀번호 틀림
+			}
+			return -1; //아이디 없음
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //오류
+	}
+	
+}
+
+/*
+ * package user;
+ * 
+ * import java.sql.Connection;
+ * import java.sql.DriverManager;
+ * import java.sql.PreparedStatement;
+ * import java.sql.ResultSet;
+ * 
+ * public class */
+ 
